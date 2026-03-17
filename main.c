3a2e606bd5e02e957a1b85139d831dbc515b3b7b
aline12marcolino-ops <aline12marcolino@gmail.com>
2026-03-17 16:17:50 -0300

    main.c

--- main.c
+++ main.c
@@ -10,9 +10,15 @@ int main() [
     printf('Erick');
     printf('Pedro'); //Pedro (ou laboratorios fiap)
     printf('Reis'); //Pedro (ou laboratorios fiap)
-    printf("Gabriel"); //Simon(No GitHub)
-    printf("Mariana");
+    printf('Gabriel'); //Simon(No GitHub)
+    printf('Mariana');
     printf("\n Sala CCPH"); //Aline
+
+// Exemplo 2: Soma de dois números  //Aline
+    int a = 5, b = 10, soma;
+    soma = a + b;
+    printf("A soma de %d + %d é: %d\n", a, b, soma);
+
  return = 0;
 ]
 
