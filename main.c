298d453b6582f7dbb33a2046de996b7b9da56a54
eduardonovaki <eduardo.novaki01@gmail.com>
2026-03-17 17:03:39 -0300

    mudei a formatação dos prints

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 
 int main() {
 
-    printf("Aline"); 
-    printf("Eduardo");
-    printf("Pedro");
-    printf("Reis");
-    printf("Gabriel");
-    printf("Mariana");
-    printf("\n sala 1CCPH");
+    printf("Aline\n"); 
+    printf("Eduardo\n");
+    printf("Pedro\n");
+    printf("Reis\n");
+    printf("Gabriel\n");
+    printf("Mariana\n");
+    printf("sala 1CCPH\n");
 
     int a = 5, b = 10, soma;
 
     soma = a + b;
 
-    printf("A soma de %d + %d é: %d\n", a, b, soma);
+    printf("A soma de %d + %d eh: %d\n", a, b, soma);
 
     return 0;
 }
