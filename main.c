790c173a3c93cf4d347fd4bb40f995efef8ae9c5
eduardonovaki <eduardo.novaki01@gmail.com>
2026-03-17 16:59:47 -0300

    Arrumei os erros do código

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 // biblioteca
 
-#include<stdio.h> // Gabriel Siqueira
- 
+#include <stdio.h> // Gabriel Siqueira
 
-// código
+//código
 
-int main() [
-    printf('Aline'); //Aline 
-    printf('Erick');
-    printf('Pedro'); //Pedro (ou laboratorios fiap)
-    printf('Reis'); //Pedro (ou laboratorios fiap)
-    printf('Gabriel'); //Simon(No GitHub)
-    printf('Mariana');
-    printf("\n Sala CCPH"); //Aline
+int main() {
 
-// Exemplo 2: Soma de dois números  //Aline
+    printf("Aline"); 
+    printf("Eduardo");
+    printf("Pedro");
+    printf("Reis");
+    printf("Gabriel");
+    printf("Mariana");
+    printf("\n sala 1CCPH");
+
     int a = 5, b = 10, soma;
+
     soma = a + b;
+
     printf("A soma de %d + %d é: %d\n", a, b, soma);
 
- return = 0;
-]
-
+    return 0;
+}
 
 
 
